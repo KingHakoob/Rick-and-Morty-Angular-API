bc56d15c98b9f9ee14d17a3a82afe25579ae6b54
jacob <jacob.dekok01@gmail.com>
2023-06-06 13:37:39 -0700

    Passing Data Using state & Retrieving it using history.state

--- src/app/cardpage/cardpage.component.ts
+++ src/app/cardpage/cardpage.component.ts
@@ -7,6 +7,8 @@ export interface ApiData {
   results?: Array<{ name?: string, image?: string, species?: string, status?: string }>
 }
 
+export interface CharData { name?: string, image?: string, species?: string, status?: string }
+
 @Component({
   selector: 'app-cardpage',
   templateUrl: './cardpage.component.html',
@@ -19,14 +21,6 @@ export class CardpageComponent {
   data: ApiData = {};
 
   constructor(private _apiservice: ApifetchService, private router: Router) { }
-
-  // goto2(){
-  //   this.router.navigate(["/child2"])
-  // }
-
-  public charPage() {
-    this.router.navigate(["/charpage"])
-  }
 
   public prevPage() {
     if (this.page > 1) {
@@ -45,7 +39,10 @@ export class CardpageComponent {
   ngOnInit() {
     this._apiservice.getData(this.page).subscribe(res => {
       this.data = res;
-      console.log(this.data);
     })
+  }
+  
+  public charPage(char: CharData) {
+      this.router.navigate(["/charpage"], {state: {data: char}})
   }
 }
--- src/app/charpage/charpage.component.ts
+++ src/app/charpage/charpage.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
+export interface CharData { name?: string, image?: string, species?: string, status?: string }
 @Component({
   selector: 'app-charpage',
   templateUrl: './charpage.component.html',
   styleUrls: ['./charpage.component.css']
 })
 export class CharpageComponent {
-
+  @Input() char: CharData = history.state.data;
+  
+  ngOnInit() {
+    console.log(this.char);
+  }
 }
